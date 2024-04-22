0ce81b55a866f2cb4f310ff1cebf674a45709899
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2024-04-22 21:00:20 +0200

    Add some new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import NewProjectInputs from "./components/NewProjectInputs";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSidebar from "./components/ProjectsSideBar";
 
 function App() {
-  return <NewProjectInputs />;
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar />
+      <NoProjectSelected />
+    </main>
+  );
 }
 
 export default App;
